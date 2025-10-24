3fecea4a6be64026314d4b2fe952d98af96e424e
donkennie <ajeigbekehinde160@gmail.com>
2025-10-24 15:13:12 +0100

    use styleForm function instead of textStyle

--- lib/currency_converter_material_page.dart
+++ lib/currency_converter_material_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 class CurrencyConverterMaterialPage extends StatelessWidget {
@@ -5,13 +6,25 @@ class CurrencyConverterMaterialPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+    int result = 0;
+    final TextEditingController textEditingController = TextEditingController();
+
     final border = OutlineInputBorder(
       borderSide: const BorderSide(width: 2.0, style: BorderStyle.solid),
-      borderRadius: BorderRadius.circular(10),
+      borderRadius: BorderRadius.circular(5),
     );
 
     return Scaffold(
       backgroundColor: Colors.blueGrey,
+      appBar: AppBar(
+        backgroundColor: Colors.blueGrey,
+        elevation: 0,
+        title: const Text(
+          'Currency Converter',
+        ),
+        centerTitle: true,
+      ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
@@ -19,7 +32,6 @@ class CurrencyConverterMaterialPage extends StatelessWidget {
             Container(
               padding: const EdgeInsets.all(10),
               margin: const EdgeInsets.all(10),
-              color: Colors.black,
               child: const Text(
                 '0',
                 style: TextStyle(
@@ -32,6 +44,7 @@ class CurrencyConverterMaterialPage extends StatelessWidget {
             Container(
               padding: const EdgeInsets.all(10),
               child: TextField(
+                controller: textEditingController,
                 style: const TextStyle(color: Colors.black),
                 decoration: InputDecoration(
                   hintText: 'Please enter the amount in USD',
@@ -46,6 +59,24 @@ class CurrencyConverterMaterialPage extends StatelessWidget {
                 keyboardType: const TextInputType.numberWithOptions(
                   decimal: true,
                 ),
+              ),
+            ),
+
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: TextButton(
+                onPressed: () {
+                  print(textEditingController);
+                },
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.black,
+                  foregroundColor: Colors.white,
+                  minimumSize: const Size(double.infinity, 50),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(5)),
+                  ),
+                ),
+                child: const Text('Click Me'),
               ),
             ),
           ],
